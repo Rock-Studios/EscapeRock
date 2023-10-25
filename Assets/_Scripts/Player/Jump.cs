d50dd71d419f15f8ae5ff3b1673fd5f4ae788a8a
Ann-Sofie <annsofiej95@hotmail.com>
2023-10-25 10:05:16 +0200

    Jump Script updated
    jump script updated

--- Assets/_Scripts/Player/Jump.cs
+++ Assets/_Scripts/Player/Jump.cs
@@ -1,35 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Jump : MonoBehaviour
 {
-    public float jumpForce = 10f; // Adjust the jump force as needed
-    private Rigidbody rb;
+    public float jumpForce = 10f;
+    public float forwardSpeed = 5f;
+
+    private Rigidbody2D rb2D; // Change this line
     private bool isGrounded;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        rb2D = GetComponent<Rigidbody2D>(); // And this line
     }
 
     void Update()
     {
-        // Check for ground detection, you can adjust this based on your game's setup
-        // You might want to use raycasting or colliders to detect if the player is grounded.
-        // Here, we are using a simple example for demonstration purposes.
-        isGrounded = Physics.Raycast(transform.position, Vector3.down, 1.1f);
+        // Check for ground detection
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, 1.1f);
 
-        if (isGrounded && Input.GetButtonDown("Jump"))
+        // Jump logic
+        if (isGrounded && Input.GetKeyDown(KeyCode.W))
         {
             JumpPlayer();
         }
+
+        // Always move forward
+        MoveForward();
+    }
+
+    void MoveForward()
+    {
+        rb2D.velocity = new Vector2(forwardSpeed, rb2D.velocity.y);
     }
 
     void JumpPlayer()
     {
-        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        rb2D.velocity = new Vector2(rb2D.velocity.x, jumpForce);
     }
 }
-
-
