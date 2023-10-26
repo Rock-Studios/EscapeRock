0b456e3d570d86c66c48d6f5852f93e0de7c67b3
Ann-Sofie <annsofiej95@hotmail.com>
2023-10-26 14:51:36 +0200

    Boulder is added
    Boulder is added

---
+++ Assets/_Scripts/Monsters/Boulder/RollingBoulder.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class RollingBoulder : MonoBehaviour
+{
+    public float speed = 5f;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        // Set the boulder's initial velocity
+        rb.velocity = new Vector2(+speed, 0); 
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            PlayerHealth playerHealth = collision.gameObject.GetComponent<PlayerHealth>();
+            playerHealth.TakeDamage(playerHealth.currentHealth);
+        }
+    }
+}
