35c32e29e88a39b15d59e31087a78f62906edc6c
Ann-Sofie <annsofiej95@hotmail.com>
2023-11-01 14:52:21 +0100

    updatess

--- Assets/_Scripts/Monsters/Boulder/RollingBoulder.cs
+++ Assets/_Scripts/Monsters/Boulder/RollingBoulder.cs
@@ -1,23 +1,40 @@
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class RollingBoulder : MonoBehaviour
 {
     public float speed = 30f;
     private Rigidbody2D rb;
+    private bool isRolling = false; // Add a boolean to control when the boulder starts rolling
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        // Set the boulder's initial velocity
-        rb.velocity = new Vector2(+speed, 0); 
+        // Initialize the boulder's velocity to zero
+        rb.velocity = Vector2.zero;
+    }
+
+    private void Update()
+    {
+        // If isRolling is true, set the boulder's velocity
+        if (isRolling)
+        {
+            rb.velocity = new Vector2(+speed, 0);
+        }
+    }
+
+    public void StartRolling()
+    {
+        isRolling = true; // This will be called by the button to start the boulder rolling
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        Debug.Log("Collision detected with: " + collision.gameObject.name);
+
+        if (collision.gameObject.tag == "Player")
         {
-            PlayerHealth playerHealth = collision.gameObject.GetComponent<PlayerHealth>();
-            playerHealth.TakeDamage(playerHealth.currentHealth);
+            Debug.Log("Player hit by boulder");
+            GameOver.instance.EndGame();
         }
     }
 }
